b3853622978c6645f46f0070fce280f8e57897a7
wheldongori <mgori089@gmail.com>
2021-06-13 00:23:54 +0300

    configured Yii2 Module

--- common/tests/_bootstrap.php
+++ common/tests/_bootstrap.php
@@ -7,3 +7,5 @@ require_once __DIR__ .  '/../../vendor/autoload.php';
 require_once __DIR__ .  '/../../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../config/bootstrap.php';
 
+new yii\web\Application(require(__DIR__ . '/../config/test.php'));
+
--- frontend/tests/_bootstrap.php
+++ frontend/tests/_bootstrap.php
@@ -7,3 +7,5 @@ require_once YII_APP_BASE_PATH . '/vendor/autoload.php';
 require_once YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php';
 require_once YII_APP_BASE_PATH . '/common/config/bootstrap.php';
 require_once __DIR__ . '/../config/bootstrap.php';
+
+new yii\web\Application(require(__DIR__ . '/../config/test.php'));
